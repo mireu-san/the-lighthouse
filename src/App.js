d08e580fd8e26e6a6835599da938d201aca5bc61
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-11-11 18:03:14 +0900

    go simple - initial point

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React from "react"
 
-// import Start from "./Components/Pages/Start/Start"
-import Skills from "./Components/Pages/Skills"
-import Projects from "./Components/Pages/Projects/Projects"
-// import Contact from "../src/Components/Pages/Contact/Contact"
-
-import { useState } from "react"
-
 function App() {
-  const [menuOpen, setMenuOpen] = useState(false)
+
 
   return (
     <div className="App">
-      <Header menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+
 
       <div className="sections">
-        {/* <Start /> */}
-        <Skills />
-        <Projects />
-        {/* <Contact /> */}
+
       </div>
     </div>
   );
