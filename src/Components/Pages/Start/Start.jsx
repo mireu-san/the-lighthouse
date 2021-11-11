569925d25ad39a28948373631dc1aba0d8f7751a
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-11-11 12:09:10 +0900

    temp version

---
+++ src/Components/Pages/Start/Start.jsx
@@ -0,0 +1,24 @@
+import "./Start.scss"
+import { useEffect, useRef } from "react"
+
+export default function Start() {
+    return (
+        <div className="start" id="start">
+            <div className="left">
+                <div className="imageContainer">
+                    <img src="#" alt="" />
+            </div>
+        </div>
+        <div className="right">
+            <div className="wrapper">
+                <h1>Greetings</h1>
+                <h1>Greetings</h1>
+                {/* consider using other tag? */}
+            </div>
+        </div>
+        <a href="#projects">
+            <img src="#" alt="" />
+        </a>
+        </div>
+    )
+}
