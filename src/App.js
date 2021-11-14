0f8f30be958dfbc3b5b8e2a6c427c0bf11509bea
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-11-15 00:25:28 +0900

    navbar is now operating!!

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from "react";
-
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home, Skills, Projects, About, Contact } from './pages';
 
 
 function App() {
   return (
-    <div>
-      <h1>Hello!</h1>
-        <Home />
-        <Skills />
-        <Projects />
-        <About />
-        <Contact />
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact component={<Home/>} />
+        <Route path="/about" component={<About/>} />
+        <Route path="/skills" component={<Skills/>} />
+        <Route path="/projects" component={<Projects/>} />
+        <Route path="/contact" component={<Contact/>} />
+      </Routes>
+    </Router>
   );
 }
 
