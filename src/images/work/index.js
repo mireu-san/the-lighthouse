676dfd9d07c6e171310c6bad7e93366dfa546b5b
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-12-08 20:41:06 +0900

    Work - temporary - safe zone

---
+++ src/images/work/index.js
@@ -0,0 +1,19 @@
+import covid from './covid.jpg';
+import jsportfolio from './jsportfolio.jpg';
+import kakaoclone from './kakaoclone.jpg';
+
+import reactmovie from './reactmovie.jpg';
+import reactportfolio from './reactportfolio.jpg';
+
+
+// if does not work in 30mins, try another solution. delete this whole page Here
+// do not waste too long time on this.
+// top priority - display logo or picture of each skills
+
+export {
+    covid,
+    jsportfolio,
+    kakaoclone,
+    reactmovie,
+    reactportfolio
+};
--- src/pages/Work.js
+++ src/pages/Work.js
@@ -2,6 +2,16 @@ import React from 'react'
 import styled from "styled-components";
 // import { StyledIndex } from '../styles/index.styled'
 import '../styles/Work.css';
+import '../images/work/';
+
+/* temporary start */
+import covid from '../images/work/covid.jpg';
+import jsproject from '../images/work/jsportfolio.jpg';
+import kakao from '../images/work/kakaoclone.jpg';
+import movie from '../images/work/reactmovie.jpg';
+import reactproject from '../images/work/reactportfolio.jpg';
+/* end of temporary section */
+
 
 const StyledWork = styled.div`
     display: flex;
@@ -15,48 +25,53 @@ function Work() {
         <StyledWork>
             <section className="work" id="work">
                 <h2 className="work__title">Work</h2>
-                    <aside className="header__wrapper__entire">
-                        
-                        <div className="header__wrapper__whiteBox">
-                            <div className="work__item-image">
-                                <img src="https://i.imgur.com/XqQZQZL.png" alt="work" />
+                    <aside className="header__wrapper">
+
+                    <div className="work__alphaColumn">
+                        <div className="work__container">
+                            <div className="work__box">
+                                <img src={covid} alt="covid tracker" /> 
                                     <a href="https://wonderful-goldstine-6adfe3.netlify.app/">
                                         <p className="textFont">Covid-19 tracker with opensource database</p>
                                     </a>
                             </div>
                         </div>
                         <div className="header__wrapper__whiteBox">
-                            <div className="work__item-image">
-                                <img src="https://i.imgur.com/XqQZQZL.png" alt="work" />
+                            <div className="work__box">
+                                <img src={reactproject} alt="react font animation portfolio" />
                                     <a href="https://quizzical-goldberg-fd83c4.netlify.app/">
                                         <p className="textFont">Previous reactjs portfolio</p>
                                     </a>
                             </div>
                         </div>
                         <div className="header__wrapper__whiteBox">
-                            <div className="work__item-image">
-                                <img src="https://i.imgur.com/XqQZQZL.png" alt="work" />
+                            <div className="work__box">
+                                <img src={jsproject} alt="js html css portfolio" />
                                     <a href="https://hardcore-chandrasekhar-2978e0.netlify.app/">
                                         <p className="textFont">Previous portfolio</p>
                                     </a>
                             </div>
                         </div>
+                    </div>
+
+                    <div className="work__betaColumn">
                         <div className="header__wrapper__whiteBox">
-                            <div className="work__item-image">
-                                <img src="https://i.imgur.com/XqQZQZL.png" alt="work" />
+                            <div className="work__box">
+                                <img src={movie} alt="react movie with openAPI database" />
                                     <a href="https://mireu-san.github.io/moviesite">
                                         <p className="textFont">Opensource Moviesite</p>
                                     </a>
                             </div>
                         </div>
                         <div className="header__wrapper__whiteBox">
-                            <div className="work__item-image">
-                                <img src="https://i.imgur.com/XqQZQZL.png" alt="work" />
+                            <div className="work__box">
+                                <img src={kakao} alt="kakao web app - limited by html/css" />
                                     <a href="https://mireu-san.github.io/Kokoa-Clone-2020/">
                                         <p className="textFont">Kakaotalk Web App</p>
                                     </a>
                             </div>
                         </div>
+                    </div>
                     </aside>
 
 {/* 
@@ -67,7 +82,7 @@ function Work() {
 
                 <div className="work__container">
                     <div className="work__item">
-                        <div className="work__item-image">
+                        <div className="work__box">
                             <img src="#" alt="Covid tracker with React.js" />
                         </div>
                         <div className="work__item-text">
@@ -82,7 +97,7 @@ function Work() {
                         </div>
                     </div>
                     <div className="work__item">
-                        <div className="work__item-image">
+                        <div className="work__box">
                             <img src="#" alt="JavaScript" />
                         </div>
                         <div className="work__item-text">
@@ -97,7 +112,7 @@ function Work() {
                         </div>
                     </div>
                     <div className="work__item">
-                        <div className="work__item-image">
+                        <div className="work__box">
                             <img src="#" alt="React.js" />
                         </div>
                         <div className="work__item-text">
@@ -112,7 +127,7 @@ function Work() {
                         </div>
                     </div>
                     <div className="work__item">
-                        <div className="work__item-image">
+                        <div className="work__box">
                             <img src="#" alt="previous portfolio page" />
                         </div>
                         <div className="work__item-text">
