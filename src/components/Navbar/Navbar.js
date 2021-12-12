521d492cb3c42b1617b178e5b4c4035338e6e9cf
mireu-san <67046863+mireu-san@users.noreply.github.com>
2021-12-13 00:22:21 +0900

    backup - checkpoint

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -13,16 +13,23 @@ class Navbar extends Component {
     render() {
         return(
             <nav className='NavbarItems'>
-                <h1 className='navbar__logo'><a href="/" style={{ textDecoration: 'none'}}>JW</a></h1>
+                
+                <h1 className='navbar__logo'>
+                    <a href="/" style={{ textDecoration: 'none'}}>JW</a>
+                </h1>
+
                 <div className='menu__icon' onClick={this.handleClick}>
                     {/* for the hamburger (bar) menu animation */}
                     <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
                 </div>
+
                 <ul className={this.state.clicked ? 'nav__menu active' : 'nav__menu '}>
                     {MenuItems.map((item, index) => {
                         return (
                             <li key={index}>
-                                <a className={item.cName} href={item.url}>
+                                <a 
+                                    className={item.cName} 
+                                    href={item.url}>
                                     {item.title}
                                 </a>
                             </li>
